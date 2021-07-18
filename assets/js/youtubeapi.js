6a256f27d170d34f033607a6aae263e880805274
MeghaWhat <meghamehreenrahman@gmail.com>
2021-07-18 10:50:23 +0800

    dgdfg

--- assets/js/youtubeapi.js
+++ assets/js/youtubeapi.js
@@ -1,51 +1,34 @@
-youtubeApiKey = "AIzaSyB599Zhnh0IGZVuo2rk6h43K_6oBU2Bs28";
+const youtubeAPI = "AIzaSyB51qZZmoDWTnFEW4PUjMr9ZikaTbNMaAY"
 
-gapi.load("client", loadClient);
-  
 function loadClient() {
-    gapi.client.setApiKey("AIzaSyAm4ehkwsMN4i0Cdita_8ls8y39Bapqe-M");
+    gapi.client.setApiKey(youtubeAPI);
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
-                function(err) { console.error("Error loading GAPI client for API", err); });
+              function(err) { console.error("Error loading GAPI client for API", err); });
 }
-//function start(){
-//    event.preventDefault();
-//
-//   mediaoutput.setAttribute("class","displayhidden");
-//}
+  // Make sure the client is loaded before calling this method.
+function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "maxResults": 1,
+      "order": "viewCount",
+      "q": "trailer",
+      "type": [
+        "video"
+      ]
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+}
 
-//var titleinput = JSON.stringify(specificTitle);
-const mediaoutput = document.getElementById("videoresult");
-const titlebutton = document.getElementById("specific-title");
-
-var request = {
-        part: "snippet",
-        order: "viewCount",
-        q: "Christmas",
-        type: "video",
-        maxResults: 1,
-};
-
-function getvideo() {
-    return gapi.client.youtube.search.list(request)
-    .then(function (response){
-        console.log("Responce",response);
-        const listResults = response.result.items;
-
-        const trailer = document.createElement("video");
-
-        if(listResults) {
-            
-            const videoId = listResults[0].id.videoId;
-            trailer.setAttribute("src","https://www.youtube.com/watch?v=" + videoId);
-            trailer.setAttribute("poster","http://i3.ytimg.com/vi/" + videoId + "/hqdefault.jpg");
-        }
-
-        mediaoutput.appendChild(trailer);
-        
-        //mediaoutput.classList.remove("displayhidden");
-    });
-};
- 
-titlebutton.addEventListener("click",getvideo());
+window.onload = function() {
+    loadClient();
+    execute();
+    gapi.load("client");
+}
 
